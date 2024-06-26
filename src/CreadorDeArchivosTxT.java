5b1c060cc3b904f226c0c2c825c70ae9ab23e15c
Nazareno Avendaño <98565832+nazarenoavendano@users.noreply.github.com>
2024-06-26 02:09:48 -0300

    Se arreglan mensaje

---
+++ src/CreadorDeArchivosTxT.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class CreadorDeArchivosTxT {
+    public static void main(String[] args) throws IOException {
+        String ruta = "C:\\Users\\nazar\\OneDrive\\Escritorio\\misArchivos\\archivo";
+        String mensaje = "Guaymallen de fruta es el caviar";
+
+        System.out.println("Mis manejos de archivos con Java");
+        System.out.println("--------------------------------");
+
+        Scanner scUno = new Scanner(System.in);
+
+        System.out.println("Por favor, ingrese la cantidad de archivos que desee crear: ");
+        int numArchivos = scUno.nextInt();
+        scUno.close();
+
+        for (int i = 1; i <= numArchivos ; i++) {
+            try {
+                FileWriter writer = new FileWriter(ruta + i + ".txt");
+                writer.write(mensaje);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("--------------------------------");
+                System.out.println("Malió sal :( ");
+                System.out.println("--------------------------------");
+            }
+        }
+        System.out.println("--------------------------------");
+        System.out.println("Se crearon los: " + numArchivos + " archivos exitosamente!!!");
+        System.out.println("--------------------------------");
+    }
+}
